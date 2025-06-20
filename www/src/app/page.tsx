97569b60ac6d638e477c942b4c86e1e538563be4
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-06-20 10:36:12 -0600

    quick update to www colors

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -2,14 +2,14 @@ import MattInitBanner from "../components/matt-init-banner";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <MattInitBanner />
-        <ol className="list-inside list-decimal text-sm/8 text-center sm:text-left font-[family-name:var(--font-geist-mono)] mx-auto">
+        <ol className="list-inside list-decimal text-sm/8 text-center sm:text-left font-mono mx-auto">
           <li>
             Get started by editing
             {" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+            <code className="bg-primary/[.05] px-1 py-0.5 rounded font-mono font-semibold">
               src/app/page.tsx
             </code>
             .
@@ -21,7 +21,7 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row mx-auto">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-muted-foreground font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://github.com/matthew-hre/matt-init"
             target="_blank"
             rel="noopener noreferrer"
@@ -29,7 +29,7 @@ export default function Home() {
             ⭐ Star matt-init
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[172px]"
+            className="rounded-full border border-solid border-muted transition-colors flex items-center justify-center hover:bg-muted hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[172px]"
             href="https://matthew-hre.com"
             target="_blank"
             rel="noopener noreferrer"
