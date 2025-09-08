294a73fa924dc251714cd82fec2b83502328edb0
Matthew Hrehirchuk <49077192+matthew-hre@users.noreply.github.com>
2025-09-08 13:03:21 -0600

    chore: bump zod to v4 (#88)

--- cli/src/templates/backend/drizzle/docker-postgres/src/lib/env.ts
+++ cli/src/templates/backend/drizzle/docker-postgres/src/lib/env.ts
@@ -3,14 +3,14 @@ import { z } from "zod";
 import { tryParseEnv } from "./try-parse-env";
 
 const EnvSchema = z.object({
-  NODE_ENV: z.string(),
-  DATABASE_URL: z.string(),
-  POSTGRES_DB: z.string(),
-  POSTGRES_USER: z.string(),
-  POSTGRES_PASSWORD: z.string(),
-  POSTGRES_PORT: z.string(),
-  BETTER_AUTH_SECRET: z.string(),
-  BETTER_AUTH_URL: z.string(),
+  NODE_ENV: z.string().nonempty(),
+  DATABASE_URL: z.string().nonempty(),
+  POSTGRES_DB: z.string().nonempty(),
+  POSTGRES_USER: z.string().nonempty(),
+  POSTGRES_PASSWORD: z.string().nonempty(),
+  POSTGRES_PORT: z.string().nonempty(),
+  BETTER_AUTH_SECRET: z.string().nonempty(),
+  BETTER_AUTH_URL: z.string().nonempty(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
--- www/src/lib/try-parse-env.ts
+++ www/src/lib/try-parse-env.ts
@@ -13,14 +13,17 @@ export function tryParseEnv<T extends ZodRawShape>(
   catch (error) {
     if (error instanceof ZodError) {
       let message = "Missing required values in .env:\n";
-      error.errors.forEach((err) => {
-        message += `${err.path[0]}\n`;
+      error.issues.forEach((err) => {
+        message += `${String(err.path[0])}\n`;
       });
 
       console.error(message);
     }
     else {
-      console.error("An unexpected error occurred while parsing environment variables:", error);
+      console.error(
+        "An unexpected error occurred while parsing environment variables:",
+        error,
+      );
     }
     process.exit(1);
   }
